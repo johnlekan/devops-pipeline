499834b5c04c30275e67fad8b21dd2f9b46cbff0
johnlekan <johnlekannig@gmail.com>
2025-09-22 17:11:04 +0100

    feat: add CI pipelines

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello  world")
+console.log("i'm learning node.js")
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("starting test file")
+setTimeout(() => {
+    console.log("inside settimeout")
+}, 1000)
+console.log("test completed")
